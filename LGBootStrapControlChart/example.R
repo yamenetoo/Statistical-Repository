c6b566abd6cd022b6c5b52cfdacbef560653fe75
yamenetoo <48772383+yamenetoo@users.noreply.github.com>
2024-04-10 12:49:37 +0330

    Create example.R

---
+++ LGBootStrapControlChart/example.R
@@ -0,0 +1,7 @@
+result <-LGBootStrapControlChart(
+                                    u = 0.99, alpha = 0.07,
+                                    theta = 2, p = 0.8, n = 5,
+                                    BootStrap_iterations = 100,
+                                    MontiCarlo_iter = 20,
+                                    ARL_rep = 100,verbos=TRUE
+                                )
